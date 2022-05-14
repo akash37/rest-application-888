f6d683ca868975856bfe5b1f5d15137a0a214b8d
akash37 <akash.gupta4237@gmail.com>
2022-05-15 00:52:26 +0100

    Exception Handling

--- Question-2/main.py
+++ Question-2/main.py
@@ -19,10 +19,14 @@ def hello_world():
 @app.route('/create_sport', methods=['POST'])
 def create_sport():
     payload = request.json
-    cur = db_connection.cursor()
-    sql, sql_tuple = get_create_sport_query(payload)
-    cur.execute(sql, sql_tuple)
-    db_connection.commit()
+    try:
+        cur = db_connection.cursor()
+        sql, sql_tuple = get_create_sport_query(payload)
+        cur.execute(sql, sql_tuple)
+        db_connection.commit()
+    except Exception as err:
+        return {"error": err}
+
     return {"status": 200}
 
 
@@ -34,119 +38,141 @@ def test_create_sport(client):
 @app.route('/create_event', methods=['POST'])
 def create_event():
     payload = request.json
-    cur = db_connection.cursor()
-    sql, sql_tuple = get_create_event_query(payload)
-    cur.execute(sql, sql_tuple)
-    db_connection.commit()
+    try:
+        cur = db_connection.cursor()
+        sql, sql_tuple = get_create_event_query(payload)
+        cur.execute(sql, sql_tuple)
+        db_connection.commit()
+    except Exception as err:
+        return {"error": err}
+
     return {"status": 200}
 
 
 @app.route('/create_selection', methods=['POST'])
 def create_selection():
     payload = request.json
-    cur = db_connection.cursor()
-    sql, sql_tuple = get_create_selection_query(payload)
-    cur.execute(sql, sql_tuple)
-    db_connection.commit()
+    try:
+        cur = db_connection.cursor()
+        sql, sql_tuple = get_create_selection_query(payload)
+        cur.execute(sql, sql_tuple)
+        db_connection.commit()
+    except Exception as err:
+        return {"error": err}
     return {"status": 200}
 
 
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get("term")
-    cur = db_connection.cursor()
-    sql = "select name from sport where active = true and name like '%" + query + "%'" \
-        " union select name from event where active = true and name like '%" + query + "%';"
-    cur.execute(sql)
-    output = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
-    results = json.dumps(output)
+    try:
+        cur = db_connection.cursor()
+        sql = "select name from sport where active = true and name like '%" + query + "%'" \
+            " union select name from event where active = true and name like '%" + query + "%';"
+        cur.execute(sql)
+        output = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+        results = json.dumps(output)
+    except Exception as err:
+        return {"error": err}
+
     return results
 
 
 @app.route('/update_sport', methods=['POST'])
 def update_sport():
     payload = request.json
-    cur = db_connection.cursor()
-    sport_id = str(payload["sport_id"])
-    sql = "update sport set name = '" + str(payload["name"]) + "', active="+str(payload["active"]) + \
-          " where sport_id=" + sport_id + ";"
-    cur.execute(sql)
-    db_connection.commit()
+    try:
+        cur = db_connection.cursor()
+        sport_id = str(payload["sport_id"])
+        sql = "update sport set name = '" + str(payload["name"]) + "', active="+str(payload["active"]) + \
+              " where sport_id=" + sport_id + ";"
+        cur.execute(sql)
+        db_connection.commit()
+    except Exception as err:
+        return {"error": err}
+
     return {"status": 200}
 
 
 @app.route('/update_event', methods=['POST'])
 def update_event():
     payload = request.json
-    cur = db_connection.cursor()
-    event_id = str(payload["event_id"])
-    sport_id = str(payload["sport_id"])
-    sql = "update event set status = '" + str(payload["status"]) + "', active = " + \
-          str(payload["active"]) + ", type='" + str(payload["type"]) + \
-          "', scheduled_start='" + str(payload["scheduled_start"]) + \
-          "', actual_start='" + str(payload["actual_start"]) + \
-          "', name='" + str(payload["name"]) + \
-          "' where event_id=" + event_id + ";"
-    print(sql)
-    cur.execute(sql)
-    if payload["active"]:
-        sql_sport = "update sport set active = true where sport_id=" + sport_id + ";"
-        cur.execute(sql_sport)
-    else:
-        sql_event = "select count(*) from  event where active=true and sport_id=" + sport_id + ";"
-        cur.execute(sql_event)
-        res = cur.fetchone()
-        print(res)
-        if res[0] >= 1:
+    try:
+        cur = db_connection.cursor()
+        event_id = str(payload["event_id"])
+        sport_id = str(payload["sport_id"])
+        sql = "update event set status = '" + str(payload["status"]) + "', active = " + \
+              str(payload["active"]) + ", type='" + str(payload["type"]) + \
+              "', scheduled_start='" + str(payload["scheduled_start"]) + \
+              "', actual_start='" + str(payload["actual_start"]) + \
+              "', name='" + str(payload["name"]) + \
+              "' where event_id=" + event_id + ";"
+        cur.execute(sql)
+        if payload["active"]:
             sql_sport = "update sport set active = true where sport_id=" + sport_id + ";"
             cur.execute(sql_sport)
         else:
-            sql_sport = "update sport set active = false where sport_id=" + sport_id + ";"
-            cur.execute(sql_sport)
+            sql_event = "select count(*) from  event where active=true and sport_id=" + sport_id + ";"
+            cur.execute(sql_event)
+            res = cur.fetchone()
+            print(res)
+            if res[0] >= 1:
+                sql_sport = "update sport set active = true where sport_id=" + sport_id + ";"
+                cur.execute(sql_sport)
+            else:
+                sql_sport = "update sport set active = false where sport_id=" + sport_id + ";"
+                cur.execute(sql_sport)
 
-    db_connection.commit()
+        db_connection.commit()
+
+    except Exception as err:
+        return {"error": err}
+
     return {"status": 200}
 
 
 @app.route('/update_selection', methods=['POST'])
 def update_selection():
     payload = request.json
-    cur = db_connection.cursor()
-    event_id = str(payload["event_id"])
-    selection_id = str(payload["selection_id"])
-    sql = "update selection set price = " + str(payload["price"]) + ", active = " +\
-          str(payload["active"]) + " where selection_id = " + selection_id + ";"
-    cur.execute(sql)
-    if payload["active"]:
-        sql_event = "update event set active = true where event_id=" + event_id + ";"
-        cur.execute(sql_event)
-    else:
-        sql_selection = "select count(*) from selection where active=true and event_id="+event_id + ";"
-        cur.execute(sql_selection)
-        res = cur.fetchone()
-        print(res)
-        if res[0] >= 1:
+    try:
+        cur = db_connection.cursor()
+        event_id = str(payload["event_id"])
+        selection_id = str(payload["selection_id"])
+        sql = "update selection set price = " + str(payload["price"]) + ", active = " +\
+              str(payload["active"]) + " where selection_id = " + selection_id + ";"
+        cur.execute(sql)
+        if payload["active"]:
             sql_event = "update event set active = true where event_id=" + event_id + ";"
             cur.execute(sql_event)
         else:
-            sql_event = "update event set active = false where event_id=" + event_id + ";"
-            cur.execute(sql_event)
+            sql_selection = "select count(*) from selection where active=true and event_id="+event_id + ";"
+            cur.execute(sql_selection)
+            res = cur.fetchone()
+            print(res)
+            if res[0] >= 1:
+                sql_event = "update event set active = true where event_id=" + event_id + ";"
+                cur.execute(sql_event)
+            else:
+                sql_event = "update event set active = false where event_id=" + event_id + ";"
+                cur.execute(sql_event)
 
-    ## Check for sport and activate and deactivate accordingly
-    select_sport_sql = "select sport_id from event where event_id=" + event_id + ";"
-    cur.execute(select_sport_sql)
-    sport_id = str(cur.fetchone()[0])
-    sql_sport = "select count(*) from event where active=true and sport_id=" + sport_id + ";"
-    cur.execute(sql_sport)
-    sport_count = cur.fetchone()
-    print("sport_count = " + str(sport_count))
-    if sport_count[0] >= 1:
-        sql_sport = "update sport set active=true where sport_id=" + sport_id + ";"
+        # Check for sport and activate and deactivate accordingly
+        select_sport_sql = "select sport_id from event where event_id=" + event_id + ";"
+        cur.execute(select_sport_sql)
+        sport_id = str(cur.fetchone()[0])
+        sql_sport = "select count(*) from event where active=true and sport_id=" + sport_id + ";"
         cur.execute(sql_sport)
-    else:
-        sql_sport = "update sport set active=false where sport_id=" + sport_id + ";"
-        cur.execute(sql_sport)
-    db_connection.commit()
+        sport_count = cur.fetchone()
+        if sport_count[0] >= 1:
+            sql_sport = "update sport set active=true where sport_id=" + sport_id + ";"
+            cur.execute(sql_sport)
+        else:
+            sql_sport = "update sport set active=false where sport_id=" + sport_id + ";"
+            cur.execute(sql_sport)
+        db_connection.commit()
+    except Exception as err:
+        return {"error": err}
+
     return {"status": 200}
 
 
